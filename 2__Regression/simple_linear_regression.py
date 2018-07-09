ab96b437edae0a3691250bd230a8bb5c82ba20bb
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-07-09 23:21:51 +0530

    Simple Linear Regression
    Started with simple linear regression

---
+++ 2__Regression/simple_linear_regression.py
@@ -0,0 +1,23 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:,0].values
+y = dataset.iloc[:,1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
