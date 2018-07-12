3da3e4586c0e881f87111c7b130791be32ee6543
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-07-12 20:20:01 +0530

    Fitting Linear Regression to training set

--- 2__Regression/simple_linear_regression.py
+++ 2__Regression/simple_linear_regression.py
@@ -8,16 +8,13 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,1].values
+y = dataset.iloc[:,-1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# Fitting Linear Regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
