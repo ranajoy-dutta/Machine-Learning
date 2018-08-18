95f3ae5b4c24cbb620deb7cc58911e97835b9668
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-08-18 16:09:56 +0530

    AIM and Dataset Added

--- 2__Regression/polynomial_linear_regression.py
+++ 2__Regression/polynomial_linear_regression.py
@@ -1,1 +1,13 @@
 # Polynomial Linear Regression
+""" 
+AIM :: Predict Salary of new employee based on model trained on salary of 
+current employees as per their positions(LEVEL).
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+dataset = pd.read_csv("Position_Salaries.csv")
+x = dataset.iloc[:, 1:2]
+y = dataset.iloc[:, 2]
